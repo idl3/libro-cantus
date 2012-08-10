a1075b336dcd8aaebde72a4bb0f147e20876f514
Ernest Sim <ernest.sim@actatlys.com>
2012-08-10 14:57:03 +0800

    Initial push for Libro Cantus

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LibroCantus::Application.config.secret_token = '9cbe74648acbf956bf5fc8261776eb1cbba6bebfd3a459162780a1798133ce079dd5ecc8e6174c04b8a91f4471e36701c62b44142e0167a5ab5b98e942be2c02'
